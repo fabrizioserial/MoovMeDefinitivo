0f75c9d01302166172f84933b9ee1191b40cc217
fabri <fabrizio.serial@ing.austral.edu.ar>
2019-11-05 11:38:00 -0300

    V2

--- app/src/main/java/com/spacetech/moovme/Mooveme.java
+++ app/src/main/java/com/spacetech/moovme/Mooveme.java
@@ -3,21 +3,18 @@ package com.spacetech.moovme;
 import java.util.ArrayList;
 
 import Repository.Repository;
-import Users.PhoneNumber;
+import Users.Operators;
 
 public class Mooveme {
 
 
     private Repository admin;
-    private static Users.Operators activeuser;
+    private static Operators activeuser;
 
     public Mooveme(ArrayList<Repository> repository){
         SplitRepositories(repository);
     }
 
-    public static void register(String name, PhoneNumber phoneNumber){
-        userMananger.add(new Users.Data(name,phoneNumber));
-    }
 
     public void SplitRepositories(ArrayList<Repository> arrayList){
         for(Repository repository:arrayList){
